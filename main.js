432a9d5651a1c7bc8c7bfac820c7dcf8fd85b5d1
MichaelMoriarty67 <michaelmoriarty67@hotmail.com>
2026-03-10 23:49:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, Tray, Menu } = require('electron/main')
+const { nativeImage } = require('electron/common')
+const { doSomething } = require('./rand')
+
+let tray = null
+
+app.whenReady().then(() => {
+    const red = nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACTSURBVHgBpZKBCYAgEEV/TeAIjuIIbdQIuUGt0CS1gW1iZ2jIVaTnhw+Cvs8/OYDJA4Y8kR3ZR2/kmazxJbpUEfQ/Dm/UG7wVwHkjlQdMFfDdJMFaACebnjJGyDWgcnZu1/lrCrl6NCoEHJBrDwEr5NrT6ko/UV8xdLAC2N49mlc5CylpYh8wCwqrvbBGLoKGvz8Bfq0QPWEUo/EAAAAASUVORK5CYII=')
+    const green = nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACOSURBVHgBpZLRDYAgEEOrEzgCozCCGzkCbKArOIlugJvgoRAUNcLRpvGH19TkgFQWkqIohhK8UEaKwKcsOg/+WR1vX+AlA74u6q4FqgCOSzwsGHCwbKliAF89Cv89tWmOT4VaVMoVbOBrdQUz+FrD6XItzh4LzYB1HFJ9yrEkZ4l+wvcid9pTssh4UKbPd+4vED2Nd54iAAAAAElFTkSuQmCC')
+
+    tray = new Tray(red)
+
+    const contextMenu = Menu.buildFromTemplate([
+        {
+      label: 'Toggle Icon',
+      type: 'checkbox',
+      click: ({ checked }) => {
+        checked ? tray.setImage(green) : tray.setImage(red)
+      }
+    }
+    ])
+
+    tray.setContextMenu(contextMenu)
+})
+
+doSomething()
---
+++ rand.js
@@ -0,0 +1,6 @@
+
+
+export function doSomething() {
+    console.log("Doing something....")
+}
+
