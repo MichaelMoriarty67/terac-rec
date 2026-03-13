7f91284de2d64c4996c67f5ba53dc5957d826675
MichaelMoriarty67 <michaelmoriarty67@hotmail.com>
2026-03-13 14:50:27 -0400

    feat: menu tray

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import path from 'path'
 import * as fs from 'fs'
-import { app, BrowserWindow, ipcMain, IpcMainEvent } from 'electron'
+import { app, BrowserWindow, ipcMain, IpcMainEvent, Menu, Tray, nativeImage, DesktopCapturerSource } from 'electron'
 
-import { getSources } from './capture'
+import { getSources, startAudioRecording, stopAudioRecording } from './capture'
+import { mergeAudioVideo } from './merge'
 
 let hiddenWindow: BrowserWindow | null = null
+let tray: Tray | null = null
+let isRecording = false
+let screens: DesktopCapturerSource[] = []
+let currScreen: DesktopCapturerSource | null = null
+let liveKitUpload: boolean = true
 
-app.whenReady().then(() => {
+// create hidden browser window to run render process
+
+
+app.whenReady().then(async () => {
+    // register ipc event handler for when video is done recording on render process
+    
+    screens = await getSources()
+    if (screens.length > 0) {
+        currScreen = screens[0] ?? null
+    }
+    
     hiddenWindow = new BrowserWindow({
         show: false,
         webPreferences: {
@@ -16,22 +32,61 @@ app.whenReady().then(() => {
         }
     })
 
+    const red = nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACTSURBVHgBpZKBCYAgEEV/TeAIjuIIbdQIuUGt0CS1gW1iZ2jIVaTnhw+Cvs8/OYDJA4Y8kR3ZR2/kmazxJbpUEfQ/Dm/UG7wVwHkjlQdMFfDdJMFaACebnjJGyDWgcnZu1/lrCrl6NCoEHJBrDwEr5NrT6ko/UV8xdLAC2N49mlc5CylpYh8wCwqrvbBGLoKGvz8Bfq0QPWEUo/EAAAAASUVORK5CYII=')
+    const green = nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACOSURBVHgBpZLRDYAgEEOrEzgCozCCGzkCbKArOIlugJvgoRAUNcLRpvGH19TkgFQWkqIohhK8UEaKwKcsOg/+WR1vX+AlA74u6q4FqgCOSzwsGHCwbKliAF89Cv89tWmOT4VaVMoVbOBrdQUz+FrD6XItzh4LzYB1HFJ9yrEkZ4l+wvcid9pTssh4UKbPd+4vED2Nd54iAAAAAElFTkSuQmCC')
+
+    tray = new Tray(red)
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Start Recording',
+            type: 'normal',
+            click: (menuItem) => {
+                if (!isRecording){
+                    // start audio and video recording
+                    startAudioRecording()
+                    hiddenWindow?.webContents.send('start-recording', screens[0]?.id)
+                    
+                    isRecording = !isRecording
+                    menuItem.label = isRecording ? 'Stop Recording' : 'Start Recording'
+                    tray?.setContextMenu(contextMenu)
+                } else {
+                    stopAudioRecording()
+                    hiddenWindow?.webContents.send('stop-recording')
+                }
+            }
+        },
+        {
+            label: 'Select Screen',
+            type: 'submenu',
+            submenu: screens.map((screen, i) => {
+                return {
+                    label: screen.name,
+                    type: 'radio',
+                    checked: i === 0 ? true : false,
+                    click: () => {
+                        currScreen = screen
+                    }
+                }
+            })
+        },
+        {
+            label: 'Upload to LiveKit',
+            type: 'checkbox',
+            checked: liveKitUpload,
+            click: (menuItem) => {
+                liveKitUpload = menuItem.checked
+            }
+            
+        }
+    ])
+    
     ipcMain.on('video-ready', (_: IpcMainEvent, buffer: ArrayBuffer) => {
         const buf = Buffer.from(buffer)
         fs.writeFileSync('/tmp/video.webm', buf)
     })
 
-    hiddenWindow.webContents.on('did-finish-load', async () => {
-        hiddenWindow?.webContents.openDevTools({ mode: 'detach' })
-        const screens = await getSources()
-
-        console.log('Sources:', screens.map(s => ({ id: s.id, name: s.name })))
-
-        hiddenWindow?.webContents.send('start-recording', screens[0]?.id)
-
-        setTimeout(() => {
-            hiddenWindow?.webContents.send('stop-recording')
-        }, 10000)
+    hiddenWindow.webContents.on('did-finish-load', () => {
+        tray?.setContextMenu(contextMenu)
     })
 
     hiddenWindow.loadFile('renderer/index.html')
